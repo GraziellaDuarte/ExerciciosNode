1ecb8ac517e320d13171e0b783682f48ecf350e0
GraziellaDuarte18 <grazielladuarte86@gmail.com>
2020-09-01 22:43:38 -0300

    Parametros

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ app.get("/sobre", function(req, res) {
 app.get("/blog", function(req, res) {
     res.send("Bem vindo ao meu Blog")
 })
-
+app.get('/ola/:nome/:cargo/:cor', function(req, res) {
+    res.send("<h1>Ola " + req.params.nome + "</h1>" + "<h2> Seu cargo e: " + req.params.cargo + "</h2" + "<h3>Sua cor favorita e: " + req.params.cor + "<h3")
+})
 
 //Servidor 
 app.listen(8081, function() {
-    console.log("Servidor rodando na url http://localhost:8081")
+    console.log("Servidor rodando...")
 })
