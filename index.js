90cdee03445dda4dbc7d2b3635aaa129760a3f80
GraziellaDuarte18 <grazielladuarte86@gmail.com>
2020-09-08 23:54:07 -0300

    Telas Html

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express")
-const { WSASERVICE_NOT_FOUND } = require("constants")
 const app = express()
 
 
 app.get("/", function(req, res) {
-    res.send("Seja Bem vindo ao meu app!")
+    res.sendfile(__dirname + "/html/index.html")
 })
 
 app.get("/sobre", function(req, res) {
@@ -14,7 +13,7 @@ app.get("/blog", function(req, res) {
     res.send("Bem vindo ao meu Blog")
 })
 app.get('/ola/:nome/:cargo/:cor', function(req, res) {
-    res.send("<h1>Ola " + req.params.nome + "</h1>" + "<h2> Seu cargo e: " + req.params.cargo + "</h2" + "<h3>Sua cor favorita e: " + req.params.cor + "<h3")
+    res.send("<h1>Ola " + req.params.nome + "</h1>" + "<h2> Seu cargo e: " + req.params.cargo + "</h2" + "<h3>Sua cor favorita e: " + req.params.cor + "</h3")
 })
 
 //Servidor 
