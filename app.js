46767635f022429077385cf71f0adb04f97bd2ec
GraziellaDuarte18 <grazielladuarte86@gmail.com>
2020-09-29 23:59:57 -0300

    telas novas

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require("express")
 const app = express()
 
-
+/*Rotas*/
 app.get("/", function(req, res) {
     res.sendfile(__dirname + "/html/index.html")
 })
 
-
-app.get("/login", function(req, res) {
-    res.sendfile(__dirname + "/html/login.html")
+app.get("/minhaSaude", function(req, res) {
+    res.sendfile(__dirname + "/html/minhaSaude.html")
 })
-
+app.get("/cartao", function(req, res) {
+    res.sendfile(__dirname + "/html/cartao.html")
+})
 app.get("/servicos", function(req, res) {
     res.sendfile(__dirname + "/html/servicos.html")
 })
 app.get("/login", function(req, res) {
     res.sendfile(__dirname + "/html/login.html")
 })
+
+//**Css**//
 app.use(express.static(__dirname + '/'));
 app.use(express.static(__dirname + '/minhasaude'));
 app.use(express.static(__dirname + '/cartao'));
