c2f101ede3333be2f7c41069db4a6a337a94df95
GraziellaDuarte18 <grazielladuarte86@gmail.com>
2020-10-05 23:50:33 -0300

    Novas Mudanças

--- app.js
+++ app.js
@@ -25,12 +25,7 @@ app.use(express.static(__dirname + '/minhasaude'));
 app.use(express.static(__dirname + '/cartao'));
 app.use(express.static(__dirname + '/servicos'));
 app.use(express.static(__dirname + '/login'));
-app.get("/blog", function(req, res) {
-    res.send("Bem vindo ao meu Blog")
-})
-app.get('/ola/:nome/:cargo/:cor', function(req, res) {
-    res.send("<h1>Ola " + req.params.nome + "</h1>" + "<h2> Seu cargo e: " + req.params.cargo + "</h2" + "<h3>Sua cor favorita e: " + req.params.cor + "</h3")
-})
+
 
 //Servidor 
 app.listen(8081, function() {
